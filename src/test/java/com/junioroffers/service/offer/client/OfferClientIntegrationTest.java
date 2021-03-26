8442d67f0c21466c10af50eaf5cde703499f3b09
Anna Kosonóg <annakosonog6@gmail.com>
2021-03-26 18:57:59 +0100

    "Integration test for OfferClient class"

--- src/test/java/com/junioroffers/service/offer/client/OfferClientIntegrationTest.java
+++ src/test/java/com/junioroffers/service/offer/client/OfferClientIntegrationTest.java
@@ -2,24 +2,22 @@ package com.junioroffers.service.offer.client;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.http.Fault;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.util.SocketUtils;
 import org.springframework.web.client.RestTemplate;
-
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
-
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.BDDAssertions.then;
 
 class OfferClientIntegrationTest extends SampleJobOffer {
     int port = SocketUtils.findAvailableTcpPort();
     WireMockServer wireMockServer;
-
 
     OfferClient service = new OfferClient("http://localhost:" + port + "/offers", closeRestTemplate());
 
@@ -73,5 +71,43 @@ class OfferClientIntegrationTest extends SampleJobOffer {
         then(service.getOffers()).containsExactlyElementsOf(Arrays.asList(aCybersource(), aJuniorDevOpsEngineer()));
     }
 
+    @Test
+    void should_return_empty_list_when_not_found_any_jobs() {
+        WireMock.stubFor(WireMock.get("/offers")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withFault(Fault.EMPTY_RESPONSE)));
+        then(service.getOffers().size()).isEqualTo(0);
+    }
 
+    @Test
+    void should_return_invalid_JSON_from_the_response() {
+        WireMock.stubFor(WireMock.get("/offers")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withFault(Fault.MALFORMED_RESPONSE_CHUNK)));
+        then(service.getOffers().size()).isEqualTo(0);
+    }
+
+    @Test
+    void should_return_error_when_get_random_date() {
+        WireMock.stubFor(WireMock.get("/offers")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withFault(Fault.RANDOM_DATA_THEN_CLOSE)));
+        then(service.getOffers().size()).isEqualTo(0);
+    }
+
+    @Test
+    void should_return_an_empty_list_when_the_connection_fails() {
+        WireMock.stubFor(WireMock.get("/offers")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withFault(Fault.CONNECTION_RESET_BY_PEER)));
+        then(service.getOffers().size()).isEqualTo(0);
+    }
 }
