c9ff54a4b4c6e3395d0dfea002174bc3a27b6995
Anna Kosonóg <annakosonog6@gmail.com>
2021-03-28 19:08:29 +0200

    "Change with pom.xml and add application.properties"

--- src/main/java/com/junioroffers/config/OfferClientConfig.java
+++ src/main/java/com/junioroffers/config/OfferClientConfig.java
@@ -1,20 +1,21 @@
 package com.junioroffers.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
 import java.time.Duration;
 
 @Configuration
 public class OfferClientConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder
-                .setConnectTimeout(Duration.ofMillis(1000))
-                .setReadTimeout(Duration.ofMillis(1000))
+    public RestTemplate restTemplate(@Value("${offer.client.config.connectTimeout}") long connectTimeout,
+                                     @Value("${offer.client.config.readTimeout}") long readTimeout) {
+        return new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofMillis(connectTimeout))
+                .setReadTimeout(Duration.ofMillis(readTimeout))
                 .build();
     }
 }
