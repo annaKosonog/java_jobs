80e4c424556c4b224004ba87b67b54df7b874ee2
Anna Kosonóg <annakosonog6@gmail.com>
2021-04-21 23:09:43 +0200

    "Task execute"

--- src/main/java/com/junioroffers/offer/scheduling/ConfigScheduler.java
+++ src/main/java/com/junioroffers/offer/scheduling/ConfigScheduler.java
@@ -1,5 +1,6 @@
 package com.junioroffers.offer.scheduling;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,5 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @Configuration
 @PropertySource("classpath:application-scheduled.properties")
+@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
 public class ConfigScheduler {
 }
--- src/main/java/com/junioroffers/offer/scheduling/HttpOfferScheduler.java
+++ src/main/java/com/junioroffers/offer/scheduling/HttpOfferScheduler.java
@@ -1,23 +1,38 @@
 package com.junioroffers.offer.scheduling;
 
+import com.junioroffers.infrastracture.model.dto.JobOfferDto;
 import com.junioroffers.infrastracture.service.offer.client.OfferClient;
+import com.junioroffers.infrastracture.service.offer.client.RemoteOfferClient;
+import com.junioroffers.offer.OfferService;
+import com.junioroffers.offer.domain.dao.Offer;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class HttpOfferScheduler {
 
-
-    private static OfferClient offer;
+    private OfferClient offer;
+    private OfferService offerService;
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     private static final Logger logger = LoggerFactory.getLogger(HttpOfferScheduler.class);
 
-    @Scheduled(fixedRateString = "${fixedRate.in.miliseconds}")
-    public void scheduleTheDownloadOfTheOfferUsingTheHttpClientWithAConstantDelay() {
 
+    @Scheduled(fixedDelayString = "${fixed.task}")
+    public void scheduleTheDownloadOfTheOfferUsingTheHttpClientWithAConstantDelay() {
+        logger.info("Start task executed at {}", dateFormat.format(new Date()));
+        final List<JobOfferDto> downloadOffers = offer.getOffers();
+        logger.info("Download offers: " + downloadOffers);
+        logger.info("Checking whether a given offer is already in the database");
+        final List<Offer> savedOffers = offerService.saveAllOffers(downloadOffers);
+        logger.info("Saved offers: " + savedOffers);
+        logger.info("Stop task executed at {}" + dateFormat.format(new Date()));
     }
 }
