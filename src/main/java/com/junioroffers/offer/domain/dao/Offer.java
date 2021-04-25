89052d44262439f64d481c6afdc3f97ce2d701de
Anna Kosonóg <annakosonog6@gmail.com>
2021-04-25 20:09:52 +0200

    "Test integration for saveAllOffers"

--- src/main/java/com/junioroffers/offer/domain/dao/Offer.java
+++ src/main/java/com/junioroffers/offer/domain/dao/Offer.java
@@ -8,6 +8,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
--- src/test/java/com/junioroffers/offer/OfferServiceSaveAllOffersWithContainerTest.java
+++ src/test/java/com/junioroffers/offer/OfferServiceSaveAllOffersWithContainerTest.java
@@ -1,5 +1,10 @@
 package com.junioroffers.offer;
 
+import com.junioroffers.JobOffersApplication;
+import com.junioroffers.infrastracture.model.dto.JobOfferDto;
+import com.junioroffers.infrastracture.service.offer.client.SampleJobOffer;
+import com.junioroffers.offer.domain.dao.Offer;
+import com.junioroffers.offer.domain.dao.SampleOffers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,11 +13,16 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import java.util.Arrays;
+import java.util.List;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(classes = JobOffersApplication.class)
 @ActiveProfiles("container")
 @Testcontainers
-public class OfferServiceSaveAllOffersWithContainerTest {
+public class OfferServiceSaveAllOffersWithContainerTest extends SampleJobOffer implements SampleOffers {
     @Container
     private static final MongoDBContainer mongoContainer = new MongoDBContainer(DockerImageName.parse("mongo"));
 
@@ -23,7 +33,32 @@ public class OfferServiceSaveAllOffersWithContainerTest {
 
     @Test
     void should_return_save_all_offers_in_database(@Autowired OfferRepository offerRepository,
-                                                   @Autowired OfferService offerService){
+                                                   @Autowired OfferService offerService) {
+
+        //GIVEN
+        final JobOfferDto specialUrlOne = aOfferDTo("Two turkey",
+                "One company",
+                "a lot of",
+                "special url");
+
+        final JobOfferDto specialUrlTwo = aOfferDTo("Game night",
+                "Two company",
+                "much",
+                "exists_url");
+
+        final Offer existsWithDatabase = objectParametersWithoutId(
+                "Three company",
+                "junior test",
+                "343232",
+                "exists_url");
+        offerRepository.save(existsWithDatabase);
+        then(offerRepository.existsByOfferUrl(existsWithDatabase.getOfferUrl())).isTrue();
+
+        //WHEN
+        final List<Offer> savedOffersList = offerService.saveAllOffers(Arrays.asList(specialUrlOne, specialUrlTwo));
+        //WHEN
+        assertThat(savedOffersList.size()).isEqualTo(1);
+        assertThat(offerRepository.existsByOfferUrl("exists_url")).isTrue();
 
     }
 
