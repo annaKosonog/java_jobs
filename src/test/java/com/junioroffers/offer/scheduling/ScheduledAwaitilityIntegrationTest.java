ded5ada0558492079468806a34d94e45ffae6877
Anna Kosonóg <annakosonog6@gmail.com>
2021-04-25 20:17:52 +0200

    "To add Awaitility dependencies pom.xml"

---
+++ src/test/java/com/junioroffers/offer/scheduling/ScheduledAwaitilityIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.junioroffers.offer.scheduling;
+
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig(ScheduledConfig.class)
+public class ScheduledAwaitilityIntegrationTest {
+}
+class ScheduledConfig {
+}
