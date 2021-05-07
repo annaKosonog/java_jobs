888d6cc255561f1c0515fda6d78f868088fbcac4
Anna Kosonóg <annakosonog6@gmail.com>
2021-05-07 16:40:05 +0200

    "Setting Up Swagger "

---
+++ src/main/java/com/junioroffers/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.junioroffers.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+@ConditionalOnProperty(value = "swagger.enabled", matchIfMissing = true)
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.junioroffers.offer"))
+                .paths(regex("/offers.*"))
+                .build();
+    }
+}
